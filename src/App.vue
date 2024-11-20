51e10ec92952313ec2403c13860ae47cf5c9cb1b
emmarp <ruiizemmanuel@gmail.com>
2024-11-19 23:32:33 -0600

    Project setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <AppBar />
+
+    <v-main>
+      <BottomNav />
+
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import AppBar from "@/components/AppBar.vue";
+import BottomNav from "@/components/BottomNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppBar,
+    BottomNav,
+  },
+};
+</script>
---
+++ src/components/BottomNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-bottom-navigation color="teal" horizontal>
+    <v-btn to="/">
+      <span>Home</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn to="/shop">
+      <span>Shop</span>
+      <v-icon>mdi-store</v-icon>
+    </v-btn>
+
+    <v-btn to="/product">
+      <span>Product</span>
+      <v-icon>mdi-shopping</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: "BottomNav",
+};
+</script>
+
+<style>
+</style>
